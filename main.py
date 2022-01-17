fe970cbce7f67eec37102c8564504d20f27675bd
pshemen <pshemen@gmail.com>
2022-01-17 20:59:51 +0100

    test

--- main.py
+++ main.py
@@ -1,81 +1,113 @@
-#i=2
+# a='python'
+# b=317
+# print('a:{}, b:{}')
+# b,a = a,b
 
-#print(type(i))
+# for numer in range (1500,2701):
+#     if numer %7==0 and numer % 5 ==0:
+#         print(numer)
 
-#f=2.767463
+# def x():
+#     print('hello word!')
+#     print ('cos')
 
-#print(type(f))
+# def x():
+#     return 3+3
+# print(x())
 
-#c= 0.5+1j
-#print(type(c))
+# def s1(x, y):
 
-#print(float(i),int (f), c)
+# def fibbonaci_numbers (n)
+#     ''' zwraca liczby fibonacciecio mniejsze od n '''
+#     wynik =[]
+#     a,b = 0,1
+#         while len(wynik) < n:
+#         wynik.append(a)
+#         a, b = b, a + b
+#     return wynik
+# x= fibbonaci_numbers (10)
 
-#a = 2>1
-
-#print(a)
-
-#print(type (a))
-
-#b= 2.71843> 3
-
-#print(b)
-#print(type(b))
-
-#znak ='a'
-
-#print(znak)
-
-#print(type(znak))
-
-#napis = 'ala makota'
-#print(napis)
-
-#print(type(napis))
-
-#napis = 'ala ma kota'
-
-#print(napis[0])
-
-#print(type(napis [0]))
-
-#print(napis[-1])
-
-#print(napis[-3:-1])
-
-#print(napis[::2])
-
-#napis = 'ala ma kota'
-
-#zmienna = 127
-
-#print(type(zmienna))
-
-#x= 'przemyslaw'
-
-#y=int(len(x)*0.5)
-#print(x[y-1:y+2])
-
-# s1='Fullstuck'
-# s2= 'python'
+# def dlugosc (x):
+#     a=0
+#     for y in x:
+#         a +=1
+#     return a
 #
-# s3=int(len(s1)/2)
-# print(s1[:s3]+s2+s1[s3:])
+# c= dlugosc('dlugosc')
+# print(c)
+#
+# def sumalisty(l):
+#     a=0
+#     for i in l:
+#         a=a+1
+#     return a
+#
+# print(a([1,2,3,4,5]))
 
-# s1= 'America'
-# s2= 'Japan'
-#
-# s3= int(len(s1)/2)
-# s4= int(len(s2)/2)
-#
-# print(s1[0]+s2[0]+s1[s3]+s2[s4]+s1[-1]+s2[-1])
-#
-# x= 'python'
-#
-# x1= x[::-1]
-#
-# print(x1)
 
+# Napisz funkcję w Pythonie, który mnoży wszystkie elementy na liście.
+
+# def iloczyn(s):
+#     a=s[0]
+#     for i in s[1:]:
+#         a*=i
+#     return a
+# v=iloczyn([99,88,77,66])
+# print(v)
+
+# Napisz funkcję w Pythonie, aby uzyskać największą liczbę z listy.
+
+# def funkcja(r):
+#     a=r[0]
+#     for x in r[1:]:
+#         if a<x:
+#             a=x
+#     return a
+# b=[2,4,6,-8,5]
+#
+# c= funkcja(b)
+# print(c)
+
+# Napisz funkcję w Pythonie, który zlicza liczbę znaków (częstotliwość znaków) w ciągu tekstowym.
+# Przykładowy ciąg tekstowy: google.com
+# Oczekiwany wynik: {'o': 3, 'g': 2, '.': 1, 'e': 1, 'l': 1, 'm': 1, 'c': 1}
+
+# def zlicznie(t):
+#     a={}
+#     for x in t:
+#         p=a.keys()
+#         if x in p:
+#             a[x]+=1
+#         else:
+#             a[x]=1
+#     return a
+# z=zlicznie('google.com')
+# print(z)
+
+
+# Napisz funkcję w Pythonie, który zlicza ciągi znaków, w których długość ciągu wynosi 2 lub więcej, a pierwszy i ostatni znak są takie same z podanej listy ciągów.
+# Przykładowa lista : ['abc', 'xyz', 'aba', '1221']
+# Oczekiwany wynik: 2
+
+# def ciag (x):
+#     licznik=0
+#     for i in x:
+#         if len(i) >=2 and i[:-1]==i[0]:
+#             licznik+=1
+#     return licznik
+# print(ciag(['abc', 'xyz', 'aba', '1221']))
+
+# Napisz funkcję w Pythonie, aby uzyskać listę posortowaną w porządku rosnącym według ostatniego elementu w każdej krotce z podanej listy niepustych krotek.
+# Przykładowa lista: [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+# Oczekiwany wynik : [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
+
+# def fun1(krotka):
+#     krotka[1]
+#
+# def sort(lista):
+#     posrtowana = sorted(lista, key=fun1)
+#     return posrtowana
+# print(sort([(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]))
 
 
 
