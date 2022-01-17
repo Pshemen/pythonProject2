df2c983fdfdbdfd1708d09abfe064da69e8c2624
pshemen <pshemen@gmail.com>
2022-01-17 18:00:14 +0100

    test

--- main.py
+++ main.py
@@ -33,3 +33,49 @@
 
 #print(type(napis))
 
+#napis = 'ala ma kota'
+
+#print(napis[0])
+
+#print(type(napis [0]))
+
+#print(napis[-1])
+
+#print(napis[-3:-1])
+
+#print(napis[::2])
+
+#napis = 'ala ma kota'
+
+#zmienna = 127
+
+#print(type(zmienna))
+
+#x= 'przemyslaw'
+
+#y=int(len(x)*0.5)
+#print(x[y-1:y+2])
+
+# s1='Fullstuck'
+# s2= 'python'
+#
+# s3=int(len(s1)/2)
+# print(s1[:s3]+s2+s1[s3:])
+
+# s1= 'America'
+# s2= 'Japan'
+#
+# s3= int(len(s1)/2)
+# s4= int(len(s2)/2)
+#
+# print(s1[0]+s2[0]+s1[s3]+s2[s4]+s1[-1]+s2[-1])
+#
+# x= 'python'
+#
+# x1= x[::-1]
+#
+# print(x1)
+
+
+
+
